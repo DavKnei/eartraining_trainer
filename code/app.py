cd58a7fabc9a50fedfce7808e1d36abb7769770f
David Kneidinger <david.kneidinger@uni-graz.at>
2025-08-26 20:08:44 +0200

    change background to white

--- code/app.py
+++ code/app.py
@@ -241,7 +241,8 @@ class EarTrainerApp(customtkinter.CTk):
 
 
 if __name__ == "__main__":
-    customtkinter.set_appearance_mode("System")
+    # customtkinter.set_appearance_mode("System")
+    customtkinter.set_appearance_mode("light")
     customtkinter.set_default_color_theme("blue")
 
     app = EarTrainerApp()
